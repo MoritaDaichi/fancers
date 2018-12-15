47d00004b8ecb57d439db22d399aabefe4ff71de
Owner <kireazi19990424@gmail.com>
2018-12-16 00:32:14 +0900

    add header base

--- app/assets/javascripts/top_index.js
+++ app/assets/javascripts/top_index.js
@@ -1,14 +1,14 @@
-$(function(){
-  $(window).on('load', function(){
-    var path = location.pathname;
-    if(path == "/"){
-      var $addBlocks = $('.block_wrap');
-      var html="";
-      for(var i = 0;i < window.innerWidth/ 10;i++){
-        var random = Math.floor( Math.random() * 3 ) + 1;
-        html = '<div class="block' + random + '"><div>';
-        $addBlocks.append(html) ;
-      }
-    }
-  })
-})
+// $(function(){
+//   $(window).on('load', function(){
+//     var path = location.pathname;
+//     if(path == "/"){
+//       var $addBlocks = $('.block_wrap');
+//       var html="";
+//       for(var i = 0;i < window.innerWidth/ 10;i++){
+//         var random = Math.floor( Math.random() * 3 ) + 1;
+//         html = '<div class="block' + random + '"><div>';
+//         $addBlocks.append(html) ;
+//       }
+//     }
+//   })
+// })
