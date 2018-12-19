bf07f716574fd624e6b7c288267aa1331d2a67e8
Owner <kireazi19990424@gmail.com>
2018-12-19 20:10:10 +0900

    finnish kari

--- app/assets/javascripts/top_index.js
+++ app/assets/javascripts/top_index.js
@@ -12,3 +12,58 @@
 //     }
 //   })
 // })
+function check_one(){
+  $(".top_index_activity__check-one").click(function(){
+    $(".top_index_activity__image-one").css({
+      'display':'block'
+    });
+    $(".top_index_activity__image-two").css({
+      'display':'none'
+    });
+    $(".top_index_activity__image-three").css({
+      'display':'none'
+    });
+  });
+}
+function check_two(){
+  $(".top_index_activity__check-two").click(function(){
+    $(".top_index_activity__image-one").css({
+      'display':'none'
+    });
+    $(".top_index_activity__image-two").css({
+      'display':'block'
+    });
+    $(".top_index_activity__image-three").css({
+      'display':'none'
+    });
+  });
+}
+
+function check_three(){
+  $(".top_index_activity__check-three").click(function(){
+    $(".top_index_activity__image-one").css({
+      'display':'none'
+    });
+    $(".top_index_activity__image-two").css({
+      'display':'none'
+    });
+    $(".top_index_activity__image-three").css({
+      'display':'block'
+    });
+  });
+}
+
+
+$(function(){
+  $(window).on('load', function(){
+    var path = location.pathname;
+    if(path = "/"){
+      $(".top_index_activity__image-one").css({
+        'display':'block'
+      });
+      check_one();
+      check_two();
+      check_three();
+    }
+  })
+})
