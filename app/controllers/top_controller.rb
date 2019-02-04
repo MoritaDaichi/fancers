34bcb9f312efe90b2649a4b8207a750dd3e27a55
Owner <kireazi19990424@gmail.com>
2019-02-04 20:07:37 +0900

    add slack contact view

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,15 @@
 class TopController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+  private
+    def slack_params
+      params.permit(:name, :message, :mail)
+      # requireは今回必要ない、keyがないから,form側でなんらかのテーブルに基づいてformを作成するとkeyが作られる
+    end
 end
